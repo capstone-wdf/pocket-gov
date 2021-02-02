f127571a5ce562056b471ef8888e28677b0949ef
Anya Dunaif <anya.dunaif@gmail.com>
2021-02-02 12:55:35 -0500

    can compare 2 members

--- src/screens/CompareMembers.js
+++ src/screens/CompareMembers.js
@@ -16,12 +16,30 @@ async function getMembers(congress, chamber) {
   }
 }
 
+async function compareTwoMembers(
+  firstmemberid,
+  secondmemberid,
+  congress,
+  chamber
+) {
+  const theUrl = `https://api.propublica.org/congress/v1/members/${firstmemberid}/votes/${secondmemberid}/${congress}/${chamber}.json`;
+  try {
+    const { data } = await axios.get(theUrl, config);
+    return data.results[0];
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// let compareTwoMemsData = await compareTwoMembers(firstMemberId, secondMemberId, congress, senate)
+
 export default function CompareMembers() {
   const [members, setMembers] = useState([]);
   const [member1, setMember1] = useState({});
   const [member2, setMember2] = useState({});
   const [visible1, setVisible1] = useState(false);
   const [visible2, setVisible2] = useState(false);
+  const [agreeData, setAgreeData] = useState(null);
 
   const openMenu1 = () => setVisible1(true);
   const closeMenu1 = () => setVisible1(false);
@@ -37,19 +55,19 @@ export default function CompareMembers() {
 
   apiCall();
 
-  const Item = ({ title, location }) => (
-    <View style={styles.item}>
-      <Text style={styles.title}>{title}</Text>
-      <Text style={styles.title}>{location}</Text>
-    </View>
-  );
+  const getComparison = async (firstMemberId, secondMemberId) => {
+    let compareTwoMemsData = await compareTwoMembers(
+      firstMemberId,
+      secondMemberId,
+      congress,
+      senate
+    );
+    setAgreeData(compareTwoMemsData);
+  };
 
-  const renderItem = ({ item }) => (
-    <Item
-      title={`${item.first_name} ${item.last_name}`}
-      location={item.state}
-    />
-  );
+  // if (member1.first_name && member2.first_name && !agreeData) {
+  //   getComparison(member1.id, member2.id);
+  // }
 
   return (
     <SafeAreaView style={styles.container}>
@@ -61,10 +79,14 @@ export default function CompareMembers() {
         {members.map((member) => {
           return (
             <Menu.Item
-              title={member.first_name}
+              title={`${member.first_name} ${member.last_name}`}
               key={member.id}
               onPress={() => {
-                setMember1({ first_name: member.first_name });
+                setMember1({
+                  first_name: member.first_name,
+                  last_name: member.last_name,
+                  id: member.id,
+                });
                 closeMenu1();
               }}
             />
@@ -79,23 +101,29 @@ export default function CompareMembers() {
         {members.map((member) => {
           return (
             <Menu.Item
-              title={member.first_name}
+              title={`${member.first_name} ${member.last_name}`}
               key={member.id}
               onPress={() => {
-                setMember2({ first_name: member.first_name });
+                setMember2({
+                  first_name: member.first_name,
+                  last_name: member.last_name,
+                  id: member.id,
+                });
                 closeMenu2();
+                // getComparison(member1.id, member2.id);
               }}
             />
           );
         })}
       </Menu>
-      <Text>{`${member1.first_name}`}</Text>
-      <Text>{`${member2.first_name}`}</Text>
-      {/* <FlatList
-        data={members}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.id}
-      /> */}
+      <Text>{`${member1.first_name} ${member1.last_name}`}</Text>
+      <Text>{`${member2.first_name} ${member2.last_name}`}</Text>
+      <Button onPress={() => getComparison(member1.id, member2.id)}>
+        Compare
+      </Button>
+      {agreeData && (
+        <Text>{`Agree percent: ${agreeData.agree_percent} | Common votes: ${agreeData.common_votes}`}</Text>
+      )}
     </SafeAreaView>
   );
 }
