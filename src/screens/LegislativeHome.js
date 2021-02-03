3903bda9a089a01654710b9ec75ebe23055d108b
Ahmad Zaki <ahmadzaki33@hotmail.com>
2021-02-03 18:02:48 -0500

    single member

--- src/screens/LegislativeHome.js
+++ src/screens/LegislativeHome.js
@@ -1,7 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function LegislativeHome({ navigation }) {
   return (
@@ -9,7 +9,11 @@ export default function LegislativeHome({ navigation }) {
       <Text>Legislative Home</Text>
       <Button
         title="Go to Compare Members Screen"
-        onPress={() => navigation.navigate('Compare')}
+        onPress={() => navigation.navigate("Compare")}
+      />
+      <Button
+        title="Go to Single Member Screen"
+        onPress={() => navigation.navigate("Single Member")}
       />
     </View>
   );
