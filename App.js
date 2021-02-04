fc6087d2051efadd49ebdb995436b4f5fbae22ef
May <mayyuhein@gmail.com>
2021-02-04 09:05:34 -0500

    Added firebase config and login register uis

--- App.js
+++ App.js
@@ -6,14 +6,28 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import LegislativeHome from './src/screens/LegislativeHome';
 import CompareMembers from './src/screens/CompareMembers';
+import { LoginScreen, HomeScreen, RegistrationScreen } from './src/screens'
+
+import firebase from 'firebase';
+import '@firebase/auth';
+import '@firebase/firestore';
+import { firebaseConfig } from "./secrets";
 
 const Stack = createStackNavigator();
 
 export default function App() {
+
+  if (!firebase.apps.length) {
+    console.log('Connected to Firebase')
+    firebase.initializeApp(firebaseConfig);
+  }
+
   return (
     <PaperProvider>
       <NavigationContainer>
         <Stack.Navigator>
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Registration" component={RegistrationScreen} />
           <Stack.Screen name="Legislative" component={LegislativeHome} />
           <Stack.Screen name="Compare" component={CompareMembers} />
         </Stack.Navigator>
