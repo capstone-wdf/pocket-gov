fbb6f8ebfb775e465ac43c1f3ec1c9ab343a3e9c
Eric Zou <ericczzou@gmail.com>
2021-02-04 14:57:09 -0500

    us map svg shown

--- src/screens/LegislativeHome.js
+++ src/screens/LegislativeHome.js
@@ -2,11 +2,16 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { Button, StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
+import USMap from "../components/USMap";
 
 export default function LegislativeHome({ navigation }) {
   return (
     <View style={styles.container}>
       <Text>Legislative Home</Text>
+      <View style={styles.map}>
+        <USMap onPress={() => console.log("hello")} />
+      </View>
+
       <Button
         title="Go to Compare Members Screen"
         onPress={() => navigation.navigate("Compare")}
@@ -20,10 +25,15 @@ export default function LegislativeHome({ navigation }) {
 }
 
 const styles = StyleSheet.create({
-  // container: {
-  //   flex: 1,
-  //   backgroundColor: '#fff',
-  //   alignItems: 'center',
-  //   justifyContent: 'center',
-  // },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  map: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+  },
 });
