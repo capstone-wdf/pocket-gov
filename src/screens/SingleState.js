add01fec9acb4fab74165ca695496d7ed5a54ff3
Eric Zou <ericczzou@gmail.com>
2021-02-05 15:36:41 -0500

    refactored individualStates SVGs

--- src/screens/SingleState.js
+++ src/screens/SingleState.js
@@ -2,28 +2,15 @@ import React from "react";
 import { View, StyleSheet, Dimensions } from "react-native";
 import { Text } from "react-native-paper";
 import { individualStates } from "../components/IndividualStates2";
-import Svg, { Defs, G } from "react-native-svg";
+import Svg, { Path, Defs, G } from "react-native-svg";
 import ZoomView from "@dudigital/react-native-zoomable-view/src/ReactNativeZoomableView";
 
 export default function SingleState({ route }) {
   const currentStatePath = individualStates[route.params.state];
 
-  const currentStateMap = (
-    <Svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="100%"
-      height="100%"
-      viewBox={`0 0 959 593`}
-    >
-      <Defs></Defs>
-      <G className="prefix__state" fill="#D0D0D0">
-        {currentStatePath}
-      </G>
-    </Svg>
-  );
   return (
     <View style={styles.container}>
-      <ZoomView style={styles.map}>{currentStateMap}</ZoomView>
+      <View style={styles.map}>{currentStatePath}</View>
       <View style={styles.reps}>
         <View>
           <Text>Senators</Text>
@@ -38,11 +25,13 @@ export default function SingleState({ route }) {
 
 const styles = StyleSheet.create({
   container: {
+    // paddingTop: "5%",
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
   },
+
   map: {
     flex: 1,
     width: Dimensions.get("window").width,
@@ -50,7 +39,7 @@ const styles = StyleSheet.create({
     zIndex: -1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "violet",
+    // backgroundColor: "violet",
   },
   reps: {
     flex: 1,
